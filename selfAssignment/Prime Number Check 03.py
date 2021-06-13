87d4e47974621f2cb44978fb75128941a1c5841b
PARK CHEOL-HEE <83874157+PARKCHEOLHEE-lab@users.noreply.github.com>
2021-06-13 17:54:46 +0900

    Delete selfAssignment directory

--- selfAssignment/Prime Number Check 03.py
+++
@@ -1,9 +0,0 @@
-cn3 = 103
-
-for i in range(1, cn3):
-    for j in range(2, i+1):
-        if i == j:              # i와 j의 값이 같아질 때 까지 for문을 반복해라.
-            print(i)
-            
-        elif i % j == 0:        # i / j를 한 나머지가 0면 반복문을 break해라
-            break
